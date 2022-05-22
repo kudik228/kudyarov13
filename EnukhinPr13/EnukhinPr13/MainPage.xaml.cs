1f7c9d78301eb9f0f520de72f18dcc5c9b1cc211
daddy <daddy@DESKTOP-UDFLTTO>
2022-05-22 16:16:42 +0300

    Добавьте файлы проекта.

---
+++ EnukhinPr13/EnukhinPr13/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EnukhinPr13
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            TapGestureRecognizer tapGesture = new TapGestureRecognizer
+            {
+                NumberOfTapsRequired = 1
+            };
+
+            int count = 0;  // счетчик нажатий
+            tapGesture.Tapped += (s, e) =>
+            {
+                count++;
+                lablePow.Text = $"Тапов: {count}, 2 в стпепени {count}: {Math.Pow(2,count)}";
+            };
+            lablePow.GestureRecognizers.Add(tapGesture);
+        }
+        private void ButtonFactorial_Clicked(object sender, EventArgs e)
+        {
+            if (ulong.TryParse(entryNumber.Text, out ulong factorial))
+            {
+                lableFactorial.TextColor = Color.Black;
+                lableFactorial.Text = $"Факториал числа {factorial}: {Factorial(factorial)}";
+
+            }
+            else
+            {
+                lableFactorial.TextColor = Color.Red;
+                lableFactorial.Text = "Вы ввели не число";
+            }
+        }
+
+        ulong Factorial(ulong number)
+        {
+            if (number <= 0)
+                return 1;
+            else
+                return number * Factorial(number - 1);
+        }
+    }
+}
